da50cf2a3fa29048e236c3036bd9e5be1b71c97e
Joseph Fernandez <joe_fernandez@hotmail.com>
2017-06-01 15:29:36 +0100

    Adding temperature profile to plotting routine

--- plot.py
+++ plot.py
@@ -5,22 +5,27 @@ from matplotlib.pylab import *
 ion()
 #xx = loadtxt("results/posx.dat")
 #yy = loadtxt("results/posy.dat")
+print("Loading ion coordinate files.")
 xx = loadtxt("posX.dat")
 yy = loadtxt("posY.dat")
 
-
 dims = shape(xx)
 frac = int(dims[1])
+xxs = zeros(30)
 xxf = zeros([frac*30])
 yyf = zeros([frac*30])
 
+
+print()"Flattenning position arrays to compose bivariate histogram.")
 for ii in range(0,30):
     xxf[ii*frac:(ii+1)*frac] = xx[ii,-1*frac:]
     yyf[ii*frac:(ii+1)*frac] = yy[ii,-1*frac:]
+    xxs[ii] = mean(xx[ii,:])
 
-xx
-yy
+del xx
+del yy
 
+print("Composing the bivariate histogram")
 gauss = 0.001
 nx, ny = 400,  200
 xl, xr = -6.0, 6.0
@@ -31,7 +36,6 @@ dy = (yt-yb)/(ny-1)
 
 xedges = arange(xl ,xr+dx, dx)
 yedges = arange(yb, yt+dy, dy)
-
 H, xedges, yedges = histogram2d(xxf, yyf, bins=(xedges, yedges))
 H=H.T
 
@@ -41,25 +45,27 @@ vals = zeros([nx, ny])
 chain = figure("chain")
 axes  = chain.add_subplot(111)
 
-
 axes.imshow(H, origin='low',
             extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
 
 axes.set_aspect(8)
 show()
-"""
-grid=np.zeros([nx, ny])
 
-for ii in range(nx):
-    print ii
-    xr = xl + dx*ii
-    for jj in range(ny):
-        f  = 0
-        yr = yb + dy*jj
-        cx = (xr-xxf)**2/(2.0*gauss)
-        cy = (yr-yyf)**2/(2.0*gauss)
-        fs  = sum(exp(-cx-cy))
-        grid[ii,jj] = fs
 
-axes.imshow(grid)
-"""
+print("Loading ion temperature profiles.")
+temps=loadtxt("temperatures.dat")
+print("Calculating temperature profile")
+tf = zeros(30)
+ns = shape(temps)
+ns = 0.2*ns[1]
+for ii in range(30):
+    tf[ii] = mean(temps[ii,-ns:])
+
+print("Plotting temperature profile.")
+tb, th = 3.0, 9.0
+txl, txr = -6.0, 6.0
+temp = figure("Temperature")
+axt = temo.add_subplot(111)
+ax.set_xlim([txl, txr])
+ax.set_ylim([tb, th])
+axes.plot(xxs, tf)
